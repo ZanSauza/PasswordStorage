77def6c12bf22f7cd0f587a40d93c1fa913c4efa
Zan <zanfilipp259@gmail.com>
2025-04-17 21:00:22 +0300

    comm 2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from utils import json_to_dict_list
 import os
-from typing import Optional
-import requests
+from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError
+from typing import Optional, List
+from json_db_lite import JSONDatabase
+
 
 
 
 path_to_json = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'passwords.json')
 
 
+
+
+
 app = FastAPI()
+
+
+class PPassword(BaseModel):
+    password_id: int
+    site: str = Field(default=..., description="Ссылка ресурса"),
+    username: str = Field(default=..., description="Логин"),
+    password: str = Field(default=..., description="Пароль"),
+    email: EmailStr = Field(default=..., description="email (необязательное поле)"),
+    Note: str = Field(default=..., description="примечание (необязательное поле)"),
+
+
+class RBPassword:
+    def __init__(self, username: str = None):
+        self.username: str = username
+
+
 
 
 @app.get("/")
@@ -18,18 +39,52 @@ async def root():
 
 
 
-@app.get("/passwords")
-async def get_all_passwords(username: Optional[str] = None, password_id: Optional[int] = None):
+@app.get("/passwords/{username}")
+async def get_all_passwords_username(request_body: RBPassword = Depends()) -> List[PPassword]:
     passwords = json_to_dict_list(path_to_json)
+    filtered_passwords = []
+    for password in passwords:
+        if password['username'] == request_body.username:
+            filtered_passwords.append(password)
 
-    if username is not None:
-        passwords = [p for p in passwords if p['username'] == username]
 
-    if password_id is not None:
-        passwords = [p for p in passwords if p['password_id'] == password_id]
-
-    return passwords
-
+    return filtered_passwords
 
 
 
+@app.post("/add_password")
+def add_student_handler(password: PPassword):
+    password_dict = password.dict()
+    check = add_password(password_dict)
+    if check:
+        return {"message": "Пароль успешно добавлен!"}
+    else:
+        return {"message": "Ошибка при добавлении пароля"}
+
+
+
+
+small_db = JSONDatabase(file_path='students.json')
+
+
+# получаем все записи
+def json_to_dict_list():
+    return small_db.get_all_records()
+
+
+# добавляем студента
+def add_password(password: dict):
+    small_db.add_records(password)
+    return True
+
+
+# обновляем данные по студенту
+def upd_password(upd_filter: dict, new_data: dict):
+    small_db.update_record_by_key(upd_filter, new_data)
+    return True
+
+
+# удаляем студента
+def dell_password(key: str, value: str):
+    small_db.delete_record_by_key(key, value)
+    return True
--- test.py
+++ test.py
@@ -1,16 +1,38 @@
 import requests
-
-def get_passwords_with_param_requests(username: str = None, password_id: int = None):
-    url = 'http://127.0.0.1:8000/passwords'
-    params = {}
-    if username:
-        params['username'] = username
-    if password_id:
-        params['password_id'] = password_id
-    response = requests.get(url, params=params)
-    return response.json()
+from utils import json_to_dict_list
+from pydantic import  ValidationError
+from typing import Optional, List
 
 
-passwords = get_passwords_with_param_requests(None, 1)
-for password in passwords:
-    print(password)
+#
+# def get_passwords_with_param_requests(username: str = None, password_id: int = None):
+#     url = 'http://127.0.0.1:8000/passwords'
+#     params = {}
+#     if username:
+#         params['username'] = username
+#     if password_id:
+#         params['password_id'] = password_id
+#     response = requests.get(url, params=params)
+#     return response.json()
+#
+#
+# passwords = get_passwords_with_param_requests(None, 1)
+# for password in passwords:
+#     print(password)
+
+url = "http://127.0.0.1:8000/add_student"
+
+
+data = {
+    "password_id": 1,
+    "site": "example.com",
+    "username": "myuser",
+    "password": "secretpass",
+    "email": "user@example.com",
+    "Note": "Это тестовая запись"
+}
+
+response = requests.post(url, json=data)
+
+print("Status Code:", response.status_code)
+print("Response JSON:", response.json())
